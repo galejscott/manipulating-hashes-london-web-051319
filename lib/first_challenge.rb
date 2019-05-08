2f3c7c842b8de5b045f237c7772375dd70faaf0b
Scott Gale <galejscott@gmail.com>
2019-05-08 17:38:37 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,8 +14,8 @@ def first_challenge
   }
 
   #your code here
-  def remove_strawberry("Freddy Mercury")
-    "Freddy Mercury".each do |attribute, data|
+  def remove_strawberry(contacts["Freddy Mercury"])
+    contacts["Freddy Mercury"].each do |attribute, data|
       if attribute == :favorite_ice_cream_flavors
         data.delete_if{|ice_cream| ice_cream == "strawberry"}
       end
