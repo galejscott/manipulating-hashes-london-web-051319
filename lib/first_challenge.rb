63bb690fc5189c8ccf93aa5bd03d4d21a099ab2c
Scott Gale <galejscott@gmail.com>
2019-05-08 17:36:09 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,7 @@ def first_challenge
   }
 
   #your code here
-  def delete_strawberry(contacts)
+
     contacts.each do |person, contact_details_hash|
     contact_details_hash.each do |attribute, data|
       if attribute == :favorite_ice_cream_flavors
