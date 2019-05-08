3a54981a68fd077215b59f7fb650ede593c6b58b
Scott Gale <galejscott@gmail.com>
2019-05-08 17:37:27 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,9 +14,8 @@ def first_challenge
   }
 
   #your code here
-
-    contacts.each do |person, contact_details_hash|
-    contact_details_hash.each do |attribute, data|
+  def remove_strawberry("Freddy Mercury")
+    "Freddy Mercury".each do |attribute, data|
       if attribute == :favorite_ice_cream_flavors
         data.delete_if{|ice_cream| ice_cream == "strawberry"}
       end
